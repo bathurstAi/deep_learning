ad2202315606e09d3149a185f35f43d28ca8bd7c
kevin <18yg18@queensu.ca>
2018-12-23 01:13:22 -0500

    need fix the sess, 0-d array?

--- indoor.py
+++ indoor.py
@@ -45,7 +45,7 @@ def get_file(file_dir):
     #split into image and label list
     image_list = list(temp[:,0])
     label_list = list(temp[:,1])
-
+    label_list = list(map(int,label_list))
     return image_list, label_list
 
 
@@ -83,15 +83,13 @@ def get_batch(image, label, image_W, image_H, batch_size, capacity):
 
 
 batch_size = 2
-capacity = 512
+capacity = 256
 img_w = 208
 img_h = 208
 
 test_dir = "C://Users//Kevin//Desktop//MMAI_894//Images//"
 image_list, label_list = get_file(test_dir)
 image_batch, label_batch = get_batch(image_list,label_list, img_w,img_h, batch_size, capacity)
-
-
 
 with tf.Session() as sess:
     i = 0
@@ -118,7 +116,7 @@ with tf.Session() as sess:
 
 # ######################################################### CODE FOR TESTING ######################
 
-########################## Testing space for file name and path ######################
+######################### Testing space for file name and path ######################
 # images = []
 # temp = []
 # labels = []
@@ -154,46 +152,73 @@ with tf.Session() as sess:
 #     labels_copy[count] = label_dic[i]
 #     count+=1
     
-
 # temp = np.array([images,labels_copy])
 # temp = temp.transpose()
 # #np.random.shuffle(temp) #randomize the images lists
 
 # image_list = list(temp[:,0])
 # label_list = list(temp[:,1])
+# label_list = list(map(int,label_list))
+
+# ########################## Testing space for batch ######################
+# batch_size = 2
+# capacity = 512
+# img_w = 208
+# img_h = 208
+
+# #################################################
+# sess = tf.InteractiveSession()
+
+# image = tf.cast(image_list, tf.string)
+# label = tf.cast(label_list, tf.int32)
+
+# #make input queue
+# input_queue = tf.train.slice_input_producer([image,label])
+# label=input_queue[1]
+
+# # filename_queue = tf.train.string_input_producer(tf.train.match_filenames_once(input_queue[0]))
+# # image_reader = tf.WholeFileReader()
+# # _, image_content = image_reader.read(filename_queue)
+# image_content = tf.read_file(input_queue[0])
+# image = tf.image.decode_jpeg(image_content, channels=3)
+# image = tf.image.resize_image_with_crop_or_pad(image, img_w, img_h)
+# image = tf.image.per_image_standardization(image)
+# image_batch, label_batch = tf.train.batch([image,label], batch_size = batch_size, num_threads = 64, capacity = capacity)
+# #tf.data.Dataset.batch([image,label],batch_size = batch_size)
+# label_batch = tf.reshape(label_batch,[batch_size])
 
 
-########################## Testing space for batch ######################
-batch_size = 2
-capacity = 512
-img_w = 208
-img_h = 208
+# sess.run(tf.initialize_all_variables())
+# coord = tf.train.Coordinator()
+# threads = tf.train.start_queue_runners(coord=coord)
+
+# ####################################################
+# image_list_a = image_list[5]
+# label_list_a = label_list[5]
+
+# sess = tf.InteractiveSession()
+
+# image = tf.cast(image_list_a, tf.string)
+# label = tf.cast(label_list_a, tf.int32)
+# #make input queue
+# image_content = tf.read_file(image)
+# image = tf.image.decode_jpeg(image_content, channels=3)
+# image = tf.image.resize_image_with_crop_or_pad(image, img_w, img_h)
+# image = tf.image.per_image_standardization(image)
+# image_batch, label_batch = tf.train.batch([image,label], batch_size = 1, num_threads = 64, capacity = capacity)
 
 
-image = tf.cast(image_list, tf.string)
-label = tf.cast(label_list, tf.int32)
+# plt.imshow(sess.run(image_batch), interpolation='nearest')
+# plt.show()
 
-#make input queue
-input_queue = tf.train.slice_input_producer([image,label])
 
-label=input_queue[1]
-image_content = tf.read_file(input_queue[0])
-image = tf.image.decode_jpeg(image_content, channels=3)
-image = tf.image.resize_image_with_crop_or_pad(image, img_w, img_h)
-image = tf.image.per_image_standardization(image)
-image_batch, label_batch = tf.train.batch([image,label], batch_size = batch_size, num_threads = 64, capacity = capacity)
-#tf.data.Dataset.batch([image,label],batch_size = batch_size)
-label_batch = tf.reshape(label_batch,[batch_size])
 
-###########test
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
-im = sess.run(image_batch)
 
-fig = plt.figure()
-fig.add_subplot(1,2,1)
-plt.imshow(im)
-fig.add_subplot(1,2,2)
-plt.imshow(img)
-plt.show()
+############################
+
+
+# image = tf.image.decode_jpeg(tf.read_file("C://Users//Kevin//Desktop//MMAI_894//Images//classroom\\classroom02.jpg"), channels=3)
+# sess = tf.InteractiveSession()
+# print(sess.run(image))
+# plt.imshow(sess.run(image), interpolation='nearest')
+# plt.show()
